979af45e0773e8a3aeedcce02db9617647ffdad4
hehouhua <15228611450@163.com>
2020-07-31 10:51:27 +0800

    完成了个人中心功能

--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -1,4 +1,6 @@
 $(function () {
+    var { layer } = layui;
+
     // 1.1 获取裁剪区域的 DOM 元素
     var $image = $('#image')
     // 1.2 配置选项
@@ -11,4 +13,95 @@ $(function () {
 
     // 1.3 创建裁剪区域
     $image.cropper(options)
+
+    /*
+    $('#btnChooseImage').on('click', function () {
+        $('#file').click()
+    })
+    // 为文件选择框绑定 change 事件
+    $('#file').on('change', function (e) {
+        // 获取用户选择的文件
+        var filelist = e.target.files
+        if (filelist.length === 0) {
+            return layer.msg('请选择照片！')
+        }
+
+        // 1. 拿到用户选择的文件
+        var file = e.target.files[0]
+        // 2. 将文件，转化为路径
+        var imgURL = URL.createObjectURL(file)
+        // 3. 重新初始化裁剪区域
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', imgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+
+    })
+    $("#btnImg").on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更换头像失败！')
+                }
+                layer.msg('更换头像成功！')
+                window.parent.getUserInfo()
+            }
+        })
+    })
+*/
+
+
+    //当点击上传按钮的时候,用js来模拟店家input:file文件筐
+    $("#btnChooseImage").on('click', function () {
+        $("#file").click();
+    })
+
+    $("#file").on('change', function (e) {
+        console.log(this.files);
+        if (this.files.length <= 0) {
+            return layer.msg("请选择文件");
+        }
+        //读取文件内容路径然后替换一下原始图片
+        var newImgURL = URL.createObjectURL(this.files[0]);
+        $image
+            .cropper('destroy') //销毁旧的裁剪区域
+            .attr('src', newImgURL)  //重新设置图片
+            .cropper(options)   //重新初始化裁剪区域
+    })
+    $("#btnUpload").on("click", function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+        $.ajax({
+            method: "post",
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status != 0) {
+                    return layer.msg("更换头像失败");
+                }
+                window.parent.getUserInfo();
+
+            }
+        })
+    })
+
 })
